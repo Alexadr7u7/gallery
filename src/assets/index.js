a18c1ebeeddf780dacc4cac8dbf192d0859955d5
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-07-28 22:54:01 -0600

    add project

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+// Gallery
+import imagen0 from "./img/imagen0.jpg";
+import imagen1 from "./img/imagen1.jpg";
+import imagen2 from "./img/imagen2.jpg";
+import imagen3 from "./img/imagen3.jpg";
+import imagen4 from "./img/imagen4.jpg";
+import imagen5 from "./img/imagen5.jpg";
+import imagen6 from "./img/imagen6.jpg";
+import imagen7 from "./img/imagen7.jpg";
+import imagen8 from "./img/imagen8.jpg";
+import imagen9 from "./img/imagen9.jpg";
+import imagen10 from "./img/imagen10.jpg";
+import imagen11 from "./img/imagen11.jpg";
+import imagen12 from "./img/imagen12.jpg";
+import imagen13 from "./img/imagen13.jpg";
+import imagen14 from "./img/imagen14.jpg";
+import imagen15 from "./img/imagen15.jpg";
+import imagen16 from "./img/imagen16.jpg";
+import imagen17 from "./img/imagen17.jpg";
+import imagen18 from "./img/imagen18.jpg";
+import imagen19 from "./img/imagen19.jpg";
+import imagen20 from "./img/imagen20.jpg";
+import imagen21 from "./img/imagen21.jpg";
+import imagen22 from "./img/imagen22.jpg";
+import imagen23 from "./img/imagen23.jpg";
+import imagen24 from "./img/imagen24.jpg";
+import imagen25 from "./img/imagen25.jpg";
+import imagen26 from "./img/imagen26.jpg";
+import imagen27 from "./img/imagen27.jpg";
+import imagen28 from "./img/imagen28.jpg";
+import imagen29 from "./img/imagen29.jpg";
+import imagen30 from "./img/imagen30.jpg";
+import imagen31 from "./img/imagen31.jpg";
+import imagen32 from "./img/imagen32.jpg";
+
+export {
+  imagen0,
+  imagen1,
+  imagen2,
+  imagen3,
+  imagen4,
+  imagen5,
+  imagen6,
+  imagen7,
+  imagen8,
+  imagen9,
+  imagen10,
+  imagen11,
+  imagen12,
+  imagen13,
+  imagen14,
+  imagen15,
+  imagen16,
+  imagen17,
+  imagen18,
+  imagen19,
+  imagen20,
+  imagen21,
+  imagen22,
+  imagen23,
+  imagen24,
+  imagen25,
+  imagen26,
+  imagen27,
+  imagen28,
+  imagen29,
+  imagen30,
+  imagen31,
+  imagen32,
+};
