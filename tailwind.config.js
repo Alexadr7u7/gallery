a7f7bbdb27662d4a88abf83fa57e759fdda4e387
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-08-08 01:02:38 -0600

    theme

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+  darkMode: "class", // Esto es CLAVE para que funcione con .dark
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
